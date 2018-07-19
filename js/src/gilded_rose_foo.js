8c514f1230f905c84a016cf47c371802fa2fa1b2
anguspettifer <angus.pettifer@gmail.com>
2018-07-19 11:27:39 +0100

    Refactor Foo

--- js/src/gilded_rose_foo.js
+++ js/src/gilded_rose_foo.js
@@ -10,10 +10,14 @@
 
   Foo.prototype.updateQuality = function () {
     if (this.sellIn > 0) {
-      this.quality -=1
+      this.quality -= 1
     } else {
       this.quality -= 2
     }
+    this.floorQualityAtZero()
+  };
+
+  Foo.prototype.floorQualityAtZero = function () {
     if (this.quality < 0) {
       this.quality = 0
     }
