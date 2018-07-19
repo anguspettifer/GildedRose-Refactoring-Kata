cd85d6ac39299c57b742bea3145e488f12a3948f
anguspettifer <angus.pettifer@gmail.com>
2018-07-19 15:37:20 +0100

    Passing unit tests backstage pass

--- js/spec/gilded_rose_backstage_pass_spec.js
+++ js/spec/gilded_rose_backstage_pass_spec.js
@@ -1,28 +1,40 @@
-describe("AgedBrie", function() {
+describe("BackstagePass", function() {
 
   it("The sellin should reduce by one", function() {
-    const agedBrie = new AgedBrie(5,10);
-    agedBrie.updateSellIn();
-    expect(agedBrie.sellIn).toEqual(4);
+    const backstagePass = new BackstagePass(5,10);
+    backstagePass.updateSellIn();
+    expect(backstagePass.sellIn).toEqual(4);
   });
 
   describe('quality', function(){
-    it("Should increase by 1 in sell by date", function() {
-      const agedBrie = new AgedBrie(5,10);
-      agedBrie.updateQuality();
-      expect(agedBrie.quality).toEqual(11);
+    it("Should increase by 1 when there are more than 10 days to sell by date", function() {
+      const backstagePass = new BackstagePass(11,10);
+      backstagePass.updateQuality();
+      expect(backstagePass.quality).toEqual(11);
     });
 
-    it("Should increase by 2 after sell by date", function() {
-      const agedBrie = new AgedBrie(0,10);
-      agedBrie.updateQuality();
-      expect(agedBrie.quality).toEqual(12);
+    it("Should increase by 2 when there are 10 days or less to sell by date", function() {
+      const backstagePass = new BackstagePass(10,10);
+      backstagePass.updateQuality();
+      expect(backstagePass.quality).toEqual(12);
+    });
+
+    it("Should increase by 3 when there are 5 days or less to sell by date", function() {
+      const backstagePass = new BackstagePass(5,10);
+      backstagePass.updateQuality();
+      expect(backstagePass.quality).toEqual(13);
+    });
+
+    it("Should fall to zero when sell by date is less than zero", function() {
+      const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", -1, 10) ]);
+      const items = gildedRose.update();
+      expect(items[0].quality).toEqual(0);
     });
 
     it("Should not rise above 50", function() {
-      const agedBrie = new AgedBrie(0,49);
-      agedBrie.updateQuality();
-      expect(agedBrie.quality).toEqual(50);
+      const backstagePass = new BackstagePass(5,49);
+      backstagePass.updateQuality();
+      expect(backstagePass.quality).toEqual(50);
     });
   });
 
---
+++ js/src/gilded_rose_backstage_pass.js
@@ -0,0 +1,36 @@
+(function(exports){
+  function BackstagePass(sellIn, quality){
+    this.sellIn = sellIn
+    this.quality = quality
+  };
+
+  BackstagePass.prototype.updateSellIn = function () {
+    this.sellIn -= 1
+  };
+
+  BackstagePass.prototype.updateQuality = function () {
+    if (this.sellIn > 10) {
+      this.qualityIncreaseOne()
+    } else if (this.sellIn > 5) {
+      this.qualityIncreaseOne()
+      this.qualityIncreaseOne()
+    } else if (this.sellIn > -1) {
+      this.qualityIncreaseOne()
+      this.qualityIncreaseOne()
+      this.qualityIncreaseOne()
+    }
+    this.capQualityAtFifty()
+  };
+
+  BackstagePass.prototype.qualityIncreaseOne = function () {
+    this.quality += 1
+  };
+
+  BackstagePass.prototype.capQualityAtFifty = function () {
+    if (this.quality > 50) {
+      this.quality = 50
+    }
+  };
+
+  exports.BackstagePass = BackstagePass;
+})(this);
