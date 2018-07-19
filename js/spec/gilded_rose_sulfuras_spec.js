956f8e62e023116f627b7c26d5d0cde96123f03e
anguspettifer <angus.pettifer@gmail.com>
2018-07-19 10:34:18 +0100

    Passsing unit tests for Sulfuras

--- js/spec/gilded_rose_sulfuras_spec.js
+++ js/spec/gilded_rose_sulfuras_spec.js
@@ -3,13 +3,13 @@ describe("Sulfuras", function() {
   it("The sellin should not change", function() {
     const sulfuras = new Sulfuras(5,10);
     sulfuras.updateSellIn();
-    expect(sulfuras.sellIn).toEqual(5);
+    expect(sulfuras.sellIn()).toEqual(5);
   });
 
   it("The quality should not change", function() {
     const sulfuras = new Sulfuras(5,10);
     sulfuras.updateQuality();
-    expect(sulfuras.quality).toEqual(5);
+    expect(sulfuras.quality).toEqual(10);
   });
 
 });
--- js/src/gilded_rose_sulfuras.js
+++ js/src/gilded_rose_sulfuras.js
@@ -1,3 +1,24 @@
 (function(exports){
-  function Sulfuras(sellin, quality)
-})
+  function Sulfuras(sellin, quality){
+    this.sellin = sellin
+    this.quality = quality
+  };
+
+  Sulfuras.prototype.updateSellIn = function () {
+    // nothing happens
+  };
+
+  Sulfuras.prototype.sellIn = function () {
+    return this.sellin
+  };
+
+  Sulfuras.prototype.updateQuality = function () {
+    // nothing happens
+  };
+
+  Sulfuras.prototype.quality = function () {
+    return this.quality
+  };
+
+  exports.Sulfuras = Sulfuras;
+})(this);
