4e16c84d4491e8f651ae913eb94fed32599f6db1
anguspettifer <angus.pettifer@gmail.com>
2018-07-21 09:37:41 +0100

    Refactor Item

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -15,13 +15,16 @@ class Shop {
 
   // Do alter this
   update() {
+
+    var choice = {
+      'Sulfuras, Hand of Ragnaros': Sulfuras,
+      'Aged Brie': AgedBrie,
+      'Backstage passes to a TAFKAL80ETC concert': BackstagePass,
+    }
+
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name == 'Sulfuras, Hand of Ragnaros') {
-        var item = new Sulfuras(this.items[i].sellIn, this.items[i].quality)
-      } else if (this.items[i].name == 'Aged Brie') {
-        var item = new AgedBrie(this.items[i].sellIn, this.items[i].quality)
-      } else if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-        var item = new BackstagePass(this.items[i].sellIn, this.items[i].quality)
+      if (choice[this.items[i].name] != undefined) {
+        var item = new choice[this.items[i].name](this.items[i].sellIn, this.items[i].quality)
       } else {
         var item = new Foo(this.items[i].sellIn, this.items[i].quality)
       }
@@ -30,26 +33,6 @@ class Shop {
       this.items[i].sellIn = item.sellIn
       this.items[i].quality = item.quality
     }
-
-    // function item_chooser(sellIn, quality){
-    //   var choice = {
-    //     'Sulfuras, Hand of Ragnaros': Sulfuras,
-    //     Give the actual constructor function, not an instance thereof
-    //     'Aged Brie': new AgedBrie(sellIn, quality),
-    //     'Backstage passes to a TAFKAL80ETC concert': new BackstagePass(sellIn, quality),
-    //   }
-    // }
-    //
-    // for (var i = 0; i < this.items.length; i++) {
-    //   console.log(item_chooser);
-    //   item_chooser(this.items[i].sellIn, this.items[i].quality)
-    //   var item = item_chooser.choice[this.items[i]].name
-    //
-    //   item.updateQuality();
-    //   item.updateSellIn();
-    //   this.items[i].sellIn = item.update.sellIn
-    //   this.items[i].quality = item.update.quality
-    // }
 
     return this.items;
   }
